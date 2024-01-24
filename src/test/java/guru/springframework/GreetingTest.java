6cc847c854e8cf2438ec64346718243095e3d37c
Zamora <maxizamora@deloitte.com>
2024-01-24 11:58:23 -0600

    hello world junitW

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,44 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.*;
+
+public class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called Once");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each....");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1(){
+        System.out.println(greeting.helloWorld("Max"));
+    }
+
+    @Test
+    void helloWorld2(){
+        System.out.println(greeting.helloWorld("Paulina"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After each....");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After - I am only called Once!!!");
+    }
+
+}
